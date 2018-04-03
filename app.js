c303e354ca577f92a4a67634dc8939f96b105e2f
Heath Burton <rubickdev@gmail.com>
2018-04-03 23:51:03 +1000

    got Ajax to work and appendnew command

---
+++ app.js
@@ -0,0 +1,44 @@
+// $("#btn").on("click", function(){
+//   $.getJSON("https://andruxnet-random-famous-quotes.p.mashape.com/", function(json) {
+//   $(".message").html(JSON.stringify(json));
+//   });
+
+const API_Key = "hhrAFEu9VLmshYEoZq51oSTYBHUIp1XokeGjsnITfzcjZD0nqK"
+
+
+function doIt() { 
+$.ajax({
+  url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
+  type: 'POST',
+  data: {},
+  dataType:'json',
+  success: function(data) { 
+    let quoteRtn = data.quote;
+    let authorRtn = data.author;
+    let categoryRtn = data.category;
+    console.log(quoteRtn + " " + data.author + " " + categoryRtn);   
+    $("#lead").replaceWith("Quote: " + quoteRtn + "\""); 
+    $("#author").text("- " + authorRtn);
+       },
+    error: function(err) { alert(err); },
+    beforeSend: function(xhr) {
+    xhr.setRequestHeader("X-Mashape-Authorization", API_Key);}
+    }); }
+    
+
+$(document).ready(function() {
+// When click on GetStarted button need to show the option buttons and display the first Quote.
+  const getStarted = document.getElementById("btn");
+  // Event Listners
+  
+  getStarted.addEventListener("click", function(){
+    document.getElementById("btnFamous").classList.remove("d-none");
+    document.getElementById("btnMovie").classList.remove("d-none");
+    doIt();
+  });
+
+
+
+
+});
+
