204fb9190f953bbe6f30d108f40f976777adf60a
Heath Burton <rubickdev@gmail.com>
2018-04-06 21:54:56 +1000

    Final copy before uploading to heroku

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
-const API_Key = "hhrAFEu9VLmshYEoZq51oSTYBHUIp1XokeGjsnITfzcjZD0nqK"
+const API_Key = "hhrAFEu9VLmshYEoZq51oSTYBHUIp1XokeGjsnITfzcjZD0nqK";
+var categoryRtn = "";
+var scoreCounter = 0;
+var guessCounter = 0;
+var authorRtn = "";
+var insertInst1 = "Click the Famous button if you think the quote is from someone famous or click the Movie button if you think it was a quote from a movie scene.";
+var score = scoreCounter + " / " + guessCounter;
 
-function doIt() { 
+function callData() { 
 $.ajax({
   url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
   type: 'POST',
@@ -9,31 +15,103 @@ $.ajax({
   dataType:'json',
   success: function(data) { 
     let quoteRtn = data.quote;
-    let authorRtn = data.author;
-    var categoryRtn = data.category;
-    console.log(quoteRtn + " " + data.author + " " + categoryRtn);   
-    $("#quote").text("Quote: " + quoteRtn + "\""); 
-    $("#author").text("- " + authorRtn);
-       },
-    error: function(err) { alert(err); },
-    beforeSend: function(xhr) {
-    xhr.setRequestHeader("X-Mashape-Authorization", API_Key);}
-    }); }
+    authorRtn = data.author;
+    categoryRtn = data.category;
+    let tweeter = document.getElementById('tweetButton');
+    let tweeter2 = document.getElementById('tweetButtonImg');
+    $("#quote").text("Quote: \"" + quoteRtn + "\""); 
+    tweeter.setAttribute('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quoteRtn + '" ' + authorRtn));
+    tweeter.setAttribute('target', '_blank');
+    tweeter2.setAttribute('title', 'Tweets the below quote');  
+    },
+  error: function(err) { alert(err); },
+  beforeSend: function(xhr) {
+  xhr.setRequestHeader("X-Mashape-Authorization", API_Key);}
+  }); }
     
-
+// This code to be run after the page loads
 $(document).ready(function() {
-// When clicking on the Begin button you need to show the option buttons and display the first Quote.
-  const getStarted = document.getElementById("begin");
-  // Event Listeners
+  const getStarted = document.getElementById("beginButton");
+  const guessFamous = document.getElementById("famousButton");
+  const guessMovie = document.getElementById("movieButton");
+  const clickNextButton = document.getElementById("nextButton");
+  const clickFinishButton = document.getElementById("finishButton");
+
+  // ### --- Event Listeners --- ### //
   
+  // Get Started button
   getStarted.addEventListener('click', function(){
-    // document.getElementById("btnFamous").classList.remove("d-none");
-    // document.getElementById("btnMovie").classList.remove("d-none");
-    doIt();
+    scoreCounter = 0;
+    guessCounter = 0;
+    score = scoreCounter + " / " + guessCounter;
+    document.getElementById('scoreValue').innerHTML = score;
+    callData();
+    document.getElementById('famousButton').classList.remove("d-none");
+    document.getElementById("movieButton").classList.remove("d-none");
+    document.getElementById("nextButton").classList.add("d-none");
+    document.getElementById("beginButton").classList.add("d-none");
+    document.getElementById("insertText").innerHTML = insertInst1;
+  });
+  
+  // Guess Famous Button
+  guessFamous.addEventListener('click', function(){
+    document.getElementById("famousButton").classList.add("d-none");
+    document.getElementById("movieButton").classList.add("d-none");
+    document.getElementById("nextButton").classList.remove("d-none");
+    document.getElementById("finishButton").classList.remove("d-none");
+    document.getElementById("finishButton").classList.remove("d-none");
+    if(categoryRtn === "Famous") {
+      scoreCounter += 1;
+      guessCounter += 1;
+      score = scoreCounter + " / " + guessCounter;
+      document.getElementById("insertText").innerHTML = "Correct this was a quote by \"" + authorRtn + '"';
+    } else {
+      document.getElementById("insertText").innerHTML = "Incorrect, this was a quote said in the movie " + authorRtn;
+      guessCounter += 1;
+      score = scoreCounter + " / " + guessCounter;
+    }
+  });
+ 
+  // Guess Movie Button
+  guessMovie.addEventListener('click', function(){
+    document.getElementById("famousButton").classList.add("d-none");
+    document.getElementById("movieButton").classList.add("d-none");
+    document.getElementById("nextButton").classList.remove("d-none");
+    document.getElementById("finishButton").classList.remove("d-none");
+    if(categoryRtn === "Movies") {
+      scoreCounter += 1;
+      guessCounter += 1;
+      score = scoreCounter + " / " + guessCounter;
+      document.getElementById("insertText").innerHTML = "Correct this was a quote in the movie \"" + authorRtn + '"';
+    } else {
+      document.getElementById("insertText").innerHTML = "Incorrect, this was said by the famous \"" + authorRtn + '"';
+      guessCounter += 1;
+      score = scoreCounter + " / " + guessCounter;
+    }
   });
 
+    // Clicking the Next Button
+    clickNextButton.addEventListener('click', function(){
+      document.getElementById("famousButton").classList.remove("d-none");
+      document.getElementById("movieButton").classList.remove("d-none");
+      document.getElementById("nextButton").classList.add("d-none");
+      document.getElementById("finishButton").classList.add("d-none");
+      document.getElementById('scoreValue').innerHTML = score;
+      document.getElementById("insertText").innerHTML = insertInst1;
+      callData();
+    });
 
-
+    // Clicking the Finish Button
+    clickFinishButton.addEventListener('click', function(){
+      document.getElementById("famousButton").classList.add("d-none");
+      document.getElementById("movieButton").classList.add("d-none");
+      document.getElementById("nextButton").classList.add("d-none");
+      document.getElementById("finishButton").classList.add("d-none");
+      document.getElementById("beginButton").classList.remove("d-none");
+      document.getElementById('scoreValue').innerHTML = score;
+      document.getElementById('quote').innerHTML = "Thank You for Playing!";
+      document.getElementById("insertText").innerHTML = "Your final Score is " + score;
+    });
 
 });
 
